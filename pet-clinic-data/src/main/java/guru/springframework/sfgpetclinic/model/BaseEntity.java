8a8b6a13644fd93e5326b82557d4626f41d21211
Wenlong Jiang <jwlbjtu@gmail.com>
2020-06-21 15:24:02 -0400

    BaseEntity implements Serializable

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class BaseEntity {
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
 
     private Long id;
 
