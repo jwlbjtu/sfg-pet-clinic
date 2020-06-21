ca9655d19125ee3f155c99e42f99c7720e369699
Wenlong Jiang <jwlbjtu@gmail.com>
2020-06-21 17:31:42 -0400

    Create index controller. closes #9

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(Model model) {
+        model.addAttribute("name", "Index Page");
+        return "index";
+    }
+}
